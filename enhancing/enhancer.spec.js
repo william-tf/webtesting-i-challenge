b4a4b3ffba3e5c5f0bcf6f3562bb2768c93dd3e5
will Fletcher <wfletch7@gmail.com>
2020-12-16 14:54:05 -0500

    finished MVP

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,36 @@
 const enhancer = require('./enhancer.js');
 // test away!
+describe("Sanity", () => {
+    test("works", () => {
+        expect(2*3).toBe(6)
+    })
+})
+
+describe("enhancer tests", () => {
+    let shovel
+    beforeEach(() => {
+        shovel = new enhancer.newItem('shovel', 50, 16)
+    })
+    it('is defined', () => {
+        expect(shovel).toBeDefined()
+    })
+    it('has repair method', () => {
+        expect(shovel.repair).toBeDefined()
+    })
+    it("repair method works", () => {
+        expect(shovel.repair(shovel)).toStrictEqual({...shovel, durability:100})
+    })
+    it("has success method", () => {
+        expect(shovel.success).toBeDefined()
+    })
+    it('has working success method', () => {
+        //expect(shovel.success(shovel)).toStrictEqual({...shovel, enchantment:20})
+    })
+    it('has fail method', () => {
+        expect(shovel.fail).toBeDefined()
+    })
+    test("fail method works", () => {
+        expect(shovel.fail(shovel)).toStrictEqual({...shovel, durability:40, enchantment:15})
+    })
+})
+
